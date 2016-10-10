4093cb14d81486b9cbb65c406db39f38ee02c572
Sebastian <sebastian.woldanski@gmail.com>
2016-10-10 22:05:04 +0000

    URL path parser added

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+var url = require("url");
+var path = require("path");
 var express = require('express');
+
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
+    const parsedURL = url.parse(req.url , true);
+    console.log(parsedURL);
+    // epoch found
+    var date = Date(path.basename(parsedURL.path)) || Date.parse(path.basename(parsedURL.path));
+    console.log(date);
+    if (!isNaN(date)) res.send(date);
+    
+    // default response
     res.send('API Basejump: Timestamp microservice');
+    //res.end()
 });
 
 app.listen(process.env.PORT, function() {
